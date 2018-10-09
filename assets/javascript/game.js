2e3542b258bbec82fa0cd1dfa43ed06868fc3ab1
Mattylaw192 <mattylaw192@gmail.com>
2018-10-08 23:03:31 -0400

    Minor updates to random number generator

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+
+
+var targetNumber = Math.floor(Math.random()*130)
+
+console.log(targetNumber);
+
+$("#targetNumberText").text(targetNumber);
+
+console.log(targetNumber);
+
+// $("counterText").text(counter);
+
+var crystalValue = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+
+var crystalValue1 = crystalValue[Math.floor(Math.random()*crystalValue.length)];
+
+console.log(crystalValue1);
+
+
+
+// console.log(counter);
+
