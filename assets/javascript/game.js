d4965962918d54ba86588bc54c094d217086803b
Mattylaw192 <mattylaw192@gmail.com>
2018-10-10 22:27:05 -0400

    Updated variables and images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,16 @@
 
+//Target number randomly generated
+var targetNumber = (Math.floor(Math.random()*120)+1);
 
-var targetNumber = Math.floor(Math.random()*130)
+//Variables for images randomly generated numbers
+var kyleNumber = (Math.floor(Math.random()*12)+1);
+var cartmanNumber = (Math.floor(Math.random()*12)+1);
+var stanNumber = (Math.floor(Math.random()*12)+1);
+var kennyNumber = (Math.floor(Math.random()*12)+1);
 
+
+console.log(kyleNumber);
+console.log(cartmanNumber);
 console.log(targetNumber);
 
 $("#targetNumberText").text(targetNumber);
@@ -10,12 +19,17 @@ console.log(targetNumber);
 
 // $("counterText").text(counter);
 
-var crystalValue = [1,2,3,4,5,6,7,8,9,10,11,12];
+var crystalValue = Math.floor(Math.random()*12);
 
+console.log(crystalValue);
 
 var crystalValue1 = crystalValue[Math.floor(Math.random()*crystalValue.length)];
 
 console.log(crystalValue1);
+
+
+
+$("#kenny").click()
 
 
 
