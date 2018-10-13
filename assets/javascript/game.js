942cb4a1254777e621dcb739122c10f4a131ae1c
Mattylaw192 <mattylaw192@gmail.com>
2018-10-12 22:33:31 -0400

    Updated js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,76 @@
 
+
+
 //Target number randomly generated
-var targetNumber = (Math.floor(Math.random()*120)+1);
+var targetNumber = (Math.floor(Math.random() * 120) + 1);
+var counter = 0;
 
 //Variables for images randomly generated numbers
-var kyleNumber = (Math.floor(Math.random()*12)+1);
-var cartmanNumber = (Math.floor(Math.random()*12)+1);
-var stanNumber = (Math.floor(Math.random()*12)+1);
-var kennyNumber = (Math.floor(Math.random()*12)+1);
-
-
+var kyleNumber = (Math.floor(Math.random() * 12) + 1);
+var cartmanNumber = (Math.floor(Math.random() * 12) + 1);
+var stanNumber = (Math.floor(Math.random() * 12) + 1);
+var kennyNumber = (Math.floor(Math.random() * 12) + 1);
 console.log(kyleNumber);
 console.log(cartmanNumber);
 console.log(targetNumber);
 
-$("#targetNumberText").text(targetNumber);
-
-console.log(targetNumber);
-
-// $("counterText").text(counter);
-
-var crystalValue = Math.floor(Math.random()*12);
-
-console.log(crystalValue);
-
-var crystalValue1 = crystalValue[Math.floor(Math.random()*crystalValue.length)];
-
-console.log(crystalValue1);
 
 
+//Scoring Variables
+// var counter;
+var wins = 0;
+var losses = 0;
+var targetText = $("#targetNumberText").text(targetNumber);
+var winsText = $("#winsText").text(wins);
+var lossesText = $("#lossesText").text(losses);
 
-$("#kenny").click()
+console.log(counter);
+
+console.log(targetText);
+
+//reset function
+function startGame() {
+    var randomNumber = (Math.floor(Math.random() * 120) + 1);
+    var targetNumber = randomNumber;
+    counter=0;
+    targetNumber = 0;
+
+}
+startGame();
+
+//Adds image values to counter
+$("#kyle").attr("imgNum", kyleNumber);
+$("#cartman").attr("imgNum", cartmanNumber);
+$("#stan").attr("imgNum", stanNumber);
+$("#kenny").attr("imgNum", kennyNumber);
 
 
+$(".character").click(function () {
 
-// console.log(counter);
+    var imgValue = ($(this).attr("imgNum"))
+    imgValue = parseInt(imgValue);
 
+    counter += imgValue;
+
+    alert(counter);
+    $("#counterText").html(counter);
+
+    console.log($(this).attr("imgNum"));
+      
+});
+
+function winloss(){
+    if (counter === targetNumber) {
+    wins++;
+    alert("You Win!");
+   
+  }
+
+  else if (counter >= targetNumber) {
+    alert("You Lose!");
+    losses++;
+    
+  }
+};
+
+console.log(winloss());
