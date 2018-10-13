74e22d8d563ccc86b6df2e1ed1a95951260f8519
Mattylaw192 <mattylaw192@gmail.com>
2018-10-13 19:20:50 -0400

    updated logic to JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,47 +2,58 @@
 
 
 //Target number randomly generated
+var randomNumber = 0;
 var targetNumber = (Math.floor(Math.random() * 120) + 1);
+console.log("Target Number Random Number" + " " + targetNumber);
 var counter = 0;
 
+
+
+
+
+
+
 //Variables for images randomly generated numbers
-var kyleNumber = (Math.floor(Math.random() * 12) + 1);
-var cartmanNumber = (Math.floor(Math.random() * 12) + 1);
-var stanNumber = (Math.floor(Math.random() * 12) + 1);
-var kennyNumber = (Math.floor(Math.random() * 12) + 1);
+var kyleNumber = 0;
+var cartmanNumber = 0;
+var stanNumber = 0;
+var kennyNumber = 0;
 console.log(kyleNumber);
 console.log(cartmanNumber);
-console.log(targetNumber);
 
+function randomizeImg() {
+
+}
 
 
 //Scoring Variables
-// var counter;
 var wins = 0;
 var losses = 0;
-var targetText = $("#targetNumberText").text(targetNumber);
+
 var winsText = $("#winsText").text(wins);
 var lossesText = $("#lossesText").text(losses);
 
 console.log(counter);
 
-console.log(targetText);
+
 
 //reset function
 function startGame() {
+    targetNumber = (Math.floor(Math.random() * 120) + 1);
+    var targetText = $("#targetNumberText").text(targetNumber);
     var randomNumber = (Math.floor(Math.random() * 120) + 1);
-    var targetNumber = randomNumber;
-    counter=0;
-    targetNumber = 0;
+    $("#counterText").text(0);
+    kyleNumber = (Math.floor(Math.random() * 12) + 1);
+    cartmanNumber = (Math.floor(Math.random() * 12) + 1);
+    stanNumber = (Math.floor(Math.random() * 12) + 1);
+    kennyNumber = (Math.floor(Math.random() * 12) + 1);
+    $("#kyle").attr("imgNum", kyleNumber);
+    $("#cartman").attr("imgNum", cartmanNumber);
+    $("#stan").attr("imgNum", stanNumber);
+    $("#kenny").attr("imgNum", kennyNumber);
 
 }
 startGame();
-
-//Adds image values to counter
-$("#kyle").attr("imgNum", kyleNumber);
-$("#cartman").attr("imgNum", cartmanNumber);
-$("#stan").attr("imgNum", stanNumber);
-$("#kenny").attr("imgNum", kennyNumber);
 
 
 $(".character").click(function () {
@@ -56,21 +67,29 @@ $(".character").click(function () {
     $("#counterText").html(counter);
 
     console.log($(this).attr("imgNum"));
-      
+    if (counter === targetNumber) {
+
+        wins++;
+        alert("You Win!");
+        var winsText = $("#winsText").text(wins);
+    }
+
+    else if(counter > targetNumber) {
+        alert("You Lose!");
+
+        losses++;
+        var lossesText = $("#lossesText").text(losses);
+        console.log(counter);
+    }
+
 });
 
-function winloss(){
-    if (counter === targetNumber) {
-    wins++;
-    alert("You Win!");
-   
-  }
+$("#buttonId").click(function () {
+    startGame();
+    // targetNumber = (Math.floor(Math.random() * 120) + 1);
+    $("#counterText").text(0);
+    counter = 0;
+    randomizeImg();
+});
 
-  else if (counter >= targetNumber) {
-    alert("You Lose!");
-    losses++;
-    
-  }
-};
 
-console.log(winloss());
